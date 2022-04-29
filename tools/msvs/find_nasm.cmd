5a2ae19cf4d25e8726a11f8e59a0bbccfe5fbe99
Himself65 <himself65@outlook.com>
2022-04-28 19:21:22 -0500

    Update find_nasm.cmd

--- tools/msvs/find_nasm.cmd
+++ tools/msvs/find_nasm.cmd
@@ -1,25 +1,25 @@
 @IF NOT DEFINED DEBUG_HELPER @ECHO OFF
 
-echo Looking for NASM
+ECHO Looking for NASM
 
-for /f "delims=" %%a IN ('where nasm 2^> NUL') DO (
-  echo NASM found in %%a
-  exit /b 0
+FOR /B "delims=" %%a IN ('where nasm 2^> NUL') DO (
+  ECHO NASM found in %%a
+  EXIT /B 0
 )
 
-for %%a in ("%ProgramFiles%\NASM\nasm.exe" "%ProgramFiles(x86)%\NASM\nasm.exe" "%LOCALAPPDATA%\bin\NASM\nasm.exe") do (
-  if exist %%a (
-    call :find-nasm %%a
-    exit /b 0
+FOR %%a IN ("%ProgramFiles%\NASM\nasm.exe" "%ProgramFiles(x86)%\NASM\nasm.exe" "%LOCALAPPDATA%\bin\NASM\nasm.exe") DO (
+  IF EXIST %%a (
+    CALL :find-nasm %%a
+    EXIT /B 0
   )
 )
-exit /b 1
+EXIT /B 1
 
 :find-nasm
-set p=%~1
+SET p=%~1
 :: Remove the last nine characters, which are "\nasm.exe"
-set p=%p:~0,-9%
-set "Path=%Path%;%p%"
-set p=
-echo NASM found in %~1
-exit /b 0
+SET p=%p:~0,-9%
+SET "Path=%Path%;%p%"
+SET p=
+ECHO NASM found in %~1
+EXIT /B 0
